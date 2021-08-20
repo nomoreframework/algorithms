4b4a1baddeabe6e3183492def7b1ac9c5078cede
nomoreframework <gleblukianenko@gmail.com>
2021-08-20 21:22:35 +0300

    added simple tests

---
+++ NexBigDigit/TESTS/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using NexBigDigit;
+
+namespace TESTS
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(21, Program.NextBiggerNumber(12));
+            Assert.Equal(531, Program.NextBiggerNumber(513));
+            Assert.Equal(2071,Program.NextBiggerNumber(2017));
+            Assert.Equal(441, Program.NextBiggerNumber(414));
+            Assert.Equal(414, Program.NextBiggerNumber(144));
+        }
+    }
+}
